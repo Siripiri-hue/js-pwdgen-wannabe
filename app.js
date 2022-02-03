90868027d7e5cd21fe67e2d11ee4c71ee5892a34
daemonprocellae@live.it <daemonprocellae@live.it>
2022-02-03 14:55:19 +0100

    Creazione delle variabili

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+// input variabili
+const userName = prompt("Qual è il tuo nome?")
+console.log(userName)
+const userSurname = prompt("Qual è il tuo cognome?")
+console.log(userSurname)
+const userColor = prompt("Qual è il tuo colore preferito?")
+console.log(userColor)
